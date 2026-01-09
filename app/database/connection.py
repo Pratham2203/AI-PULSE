b7b06e277348404bc744aae6ddb8aa238fb4a8eb
Pratham Goel <pratham.goel.22.183@gmail.com>
2026-01-09 17:00:43 +0530

    change username and password

--- app/database/connection.py
+++ app/database/connection.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_database_url() -> str:
-    user = os.getenv("POSTGRES_USER", "ai_news_user")
-    password = os.getenv("POSTGRES_PASSWORD", "password123")
+    user = os.getenv("POSTGRES_USER", "postgres")
+    password = os.getenv("POSTGRES_PASSWORD", "postgres")
     host = os.getenv("POSTGRES_HOST", "localhost")
     port = os.getenv("POSTGRES_PORT", "5432")
     db = os.getenv("POSTGRES_DB", "ai_news_aggregator")
