6ab21e13f6540e33fc2873a46adb009c718a6a1e
Pratham Goel <pratham.goel.22.183@gmail.com>
2026-01-09 17:53:11 +0530

    add new utube channel

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 YOUTUBE_CHANNELS = [
-    "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
-    "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
-    "UCCd3-JX7e8uGZx00i5646jg", # daily tech news
+    # "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
+    # "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+    # "UCCd3-JX7e8uGZx00i5646jg", # daily tech news
+    "UCNU_lfiiWBdtULKOw6X0Dig",
 ]
 
